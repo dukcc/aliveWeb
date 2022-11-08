7e7adef8decacb3e4ff9c885c214d4bce0a83c58
Jacob <dokygamr@outlook.com>
2022-11-07 18:32:49 -0800

    Created home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 // These styles apply to every route in the application
+import Image from 'next/image';
 import './globals.css';
 
 export default function RootLayout({ children }: {
@@ -6,7 +7,17 @@ export default function RootLayout({ children }: {
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="fixed bg-black/60 text-white backdrop-blur-sm w-[98vw] p-5 m-3 rounded-xl shadow-lg flex content-between align-middle">
+          <Image
+            src="/logo.png"
+            width={50}
+            height={50}
+            alt="Logo"
+          />
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Page() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello, Next.js!
-    </h1>
-  )
+
+	return (
+		<>
+    <head>
+        <link rel="icon" href="../img/logo.png" type="image/png" />
+        <title>Alive SMP: Home</title>
+    </head>
+		<div className="bg-[url('/bg.png')] bg-cover bg-center text-white h-screen align-middle justify-center grid place-content-center text-center">
+			<h1 className="text-7xl font-default font-bold max-[600px]:text-5xl">
+				Alive SMP
+			</h1>
+			<p className="text-xl font-default pt-3 max-[600px]:text-sm">
+				A fun, semi-vanilla Minecraft SMP.<br></br>
+        Apply to join at our Discord.
+			</p>
+		</div>
+		<div className="bg-black bg-cover bg-center text-white h-[250rem] place-items-center">
+      <Link href="https://discord.gg/jazHytWPNw" className="bg-black">
+        <div className="w-[50rem] h-[25rem] bg-[#5170ef] p-10 m-3">
+          <h1 className="text-5xl font-bold">
+            Join Our Discord
+          </h1>
+        </div>
+      </Link>
+		</div>
+		</>
+	)
 }
