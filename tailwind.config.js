6f2ba92b70575cb6cad09341c207fc5c8624f864
Jacob <dokygamr@outlook.com>
2023-01-05 19:11:21 -0800

    Fixed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                default: ['"Be Vietnam Pro"'],
+                default: ['Montserrat'],
             },
         },
     },
