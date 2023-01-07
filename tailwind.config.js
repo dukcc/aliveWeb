061fd4b669f5eb296c68cff7bb9a19ab24ddb43e
Jacob <dokygamr@outlook.com>
2023-01-06 16:32:12 -0800

    Changed font back

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                default: ['Montserrat'],
+                default: ['"Be Vietnam Pro"'],
             },
         },
     },
